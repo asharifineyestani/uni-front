236d5a320217dd20de0cbf5a014c8db27bdb8125
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-04-16 19:35:40 +0430

    update Register page

--- src/scenes/Auth/SignIn.js
+++ src/scenes/Auth/SignIn.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Row, Col } from 'react-grid-system'
 import Heading from '../../components/Heading'
 import Content from '../../components/Content'
+import api from '../../api'
 
 const SignIn = () => {
+
+    const [data, setData] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let form = {}
+        let name = e.target.name.value;
+        let mobile = e.target.mobile.value;
+        let password = e.target.password.value;
+
+        form.name = name
+        form.mobile = mobile
+        form.password = password
+
+        let formData = JSON.stringify(form)
+
+        setData(formData)
+
+        axios.post(`${api}/auth/register`, formData)
+            .then(result => {
+                console.log(result);
+                console.log(result.data);
+            });
+    }
+
     return (
         <section className="section section-auth">
             <Content>
                 <Heading title="ورود به سایت" subtitle="با بیش از 7 سال سابقه درخشان در عرصه اپلیکیش نویسی" />
-                <div className="form">
-                    <div className="form-div">
-                        <input type="text" placeholder="پست الکترونیک"/>
-                    </div>
-                    <div className="form-div">
-                        <input type="text" placeholder="رمز کاربری"/>
-                    </div>
-                    <div className="form-div">
-                        <button className="button button-md button-block button-primary">ثبت نام</button>
-                    </div>
-                </div>
+                <form className="form" onSubmit={handleSubmit}>
+                    <Row className="row-sm">
+                        <Col lg={6}>
+                            <div className="form-div">
+                                <input type="text" name="mobile" placeholder="شماره تلفن همراه" />
+                            </div>
+                        </Col>
+                        <Col lg={6}>
+                            <div className="form-div">
+                                <input type="password" name="password" placeholder="رمز کاربری" />
+                            </div>
+                        </Col>
+                    </Row>
+                    <button className="button button-md button-block button-primary" type="submit">ورود به سایت</button>
+                </form>
             </Content>
         </section>
     )
--- src/scenes/Auth/SignUp.js
+++ src/scenes/Auth/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Row, Col } from 'react-grid-system'
 import Heading from '../../components/Heading'
@@ -7,54 +7,96 @@ import api from '../../api'
 
 const SignUp = () => {
 
-    const [data, setData] = useState({});
+    const [data, setData] = useState({})
+    const [response, setResponse] = useState({})
+
+    function Form1() {
+        return (
+            <form className="form" onSubmit={handleSubmit}>
+                <Row className="row-sm">
+                    <Col lg={12}>
+                        <div className="form-div">
+                            <input type="text" name="name" placeholder="نام و نام خانوادگی" />
+                        </div>
+                    </Col>
+                    <Col lg={6}>
+                        <div className="form-div">
+                            <input type="text" name="mobile" placeholder="شماره تلفن همراه" />
+                        </div>
+                    </Col>
+                    <Col lg={6}>
+                        <div className="form-div">
+                            <input type="password" name="password" placeholder="رمز کاربری" />
+                        </div>
+                    </Col>
+                </Row>
+                <button className="button button-md button-block button-primary" type="submit">ثبت نام</button>
+            </form>
+        )
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
         let form = {}
-        let name = e.target.name.value;
-        let mobile = e.target.mobile.value;
-        let password = e.target.password.value;
+        form.name = e.target.name.value;
+        form.mobile = e.target.mobile.value;
+        form.password = e.target.password.value;
 
-        form.name = name
-        form.mobile = mobile
-        form.password = password
-
-        let formData = JSON.stringify(form)
-
-        setData(formData)
-
-        axios.post(`${api}/auth/register`, formData)
+        axios.post(`${api}/auth/register`, form)
             .then(result => {
-                console.log(result);
-                console.log(result.data);
+                setData(result.data)
+            }).catch(error => {
+                console.log(error)
             });
+    }
+
+    function Form2() {
+
+        let message = data.message
+
+        return (
+            <>
+                <h3>{message}</h3>
+                <form className="form" onSubmit={handleSubmit2}>
+                    <Row className="row-sm">
+                        <Col lg={12}>
+                            <div className="form-div">
+                                <input type="text" name="code" placeholder="کد ارسال شده را وارد نمایید" />
+                            </div>
+                        </Col>
+                    </Row>
+                    <button className="button button-md button-block button-primary" type="submit">تکمیل ثبت نام</button>
+                </form>
+            </>
+        )
+    }
+
+    const handleSubmit2 = (e) => {
+        e.preventDefault()
+        let code = e.target.code.value
+        let form = {}
+        form.code = code
+        form.mobile = data.data.mobile
+
+        if (code == data.data.code) {
+            alert("ثبت نام شما با موفقیت تکمیل شد")
+
+            axios.post(`${api}/auth/active/code`, form)
+                .then(result => {
+                    setResponse(result.data.data)
+                }).catch(error => {
+                    console.log(error)
+                });
+        }
     }
 
     return (
         <section className="section section-auth">
+            {console.log(response)}
             <Content>
                 <Heading title="ثبت نام در سایت" subtitle="با بیش از 7 سال سابقه درخشان در عرصه اپلیکیش نویسی" />
-                <form className="form" onSubmit={handleSubmit}>
-                    <Row className="row-sm">
-                        <Col lg={12}>
-                            <div className="form-div">
-                                <input type="text" name="name" placeholder="نام و نام خانوادگی" />
-                            </div>
-                        </Col>
-                        <Col lg={6}>
-                            <div className="form-div">
-                                <input type="text" name="mobile" placeholder="شماره تلفن همراه" />
-                            </div>
-                        </Col>
-                        <Col lg={6}>
-                            <div className="form-div">
-                                <input type="password" name="password" placeholder="رمز کاربری" />
-                            </div>
-                        </Col>
-                    </Row>
-                    <button className="button button-md button-block button-primary" type="submit">ثبت نام</button>
-                </form>
+                {(Object.keys(data).length === 0 && data.constructor === Object) ? <Form1 /> : <Form2 />}
             </Content>
         </section>
     )
